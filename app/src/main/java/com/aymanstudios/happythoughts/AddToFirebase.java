f9340a7102418d74d8f5caaace078726dab53eca
Mahdi Ayman <damahdi03@gmail.com>
2020-06-03 03:12:05 -0700

    Added script to add more sayings to teh Firebase Database to just Copy and Paste into the main thread

---
+++ app/src/main/java/com/aymanstudios/happythoughts/AddToFirebase.java
@@ -0,0 +1,30 @@
+package com.aymanstudios.happythoughts;
+
+import java.util.List;
+
+public class AddToFirebase {
+    /*
+    class ListOfHappyThoughtsClass {
+        //List (complicated Array) of sayings to choose from
+        private List<String> listOfHappyThoughts;
+        public ListOfHappyThoughtsClass() {
+            //Required for DataSnapshot.getValue(ListOfHappyThoughts.class)
+        }
+        //Main Setter for class
+        public ListOfHappyThoughtsClass(List<String> listOfHappyThoughts) {
+            this.listOfHappyThoughts = listOfHappyThoughts;
+        }
+        //Main Getter for class
+        public List<String> getListOfHappyThoughts() {
+            return listOfHappyThoughts;
+        }
+    }
+    //Actually use class to define variable if used from online
+    private ListOfHappyThoughtsClass listOfHappyThoughtsClass;
+    //Final List to choose sayings from (could be updated or from fallback list)
+    public List<String> listOfHappyThoughts;
+
+    ListOfHappyThoughtsClass listOfHappyThoughtsClass = new ListOfHappyThoughtsClass(Arrays.asList("string1", "string2"));
+    //db.collection("happyThoughts").document("happyThoughts").update("listOfHappyThoughts", listOfHappyThoughtsClass);*/
+
+}
