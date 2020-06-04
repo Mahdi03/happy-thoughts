386fed6bf3fd76fee8e64f4b8b3d2edb33d5c32d
Mahdi Ayman <damahdi03@gmail.com>
2020-06-03 17:22:54 -0700

    Added a splashscreen to the app

---
+++ app/src/main/java/com/aymanstudios/happythoughts/Splashscreen.java
@@ -0,0 +1,29 @@
+package com.aymanstudios.happythoughts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        /*
+        Similar to a setTimeout where this screen will be the main screen until
+        5 seconds pass and the user will be taken to the next screen
+         */
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Creates a new Intent to call the MainActivity.java
+                Intent mainActivityIntent = new Intent(Splashscreen.this, MainActivity.class);
+                startActivity(mainActivityIntent);
+                finish();
+            }
+        }, 2000);
+    }
+}
